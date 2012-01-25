76c8e4f66b410a156826acf983fb114188af5bc7
niku_uchi <mitsu.longsprint.int@gmail.com>
2012-01-25 09:57:36 +0900

    start 0020

---
+++ 0020.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#include <list>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <sstream>
+#include <numeric>
+#include <utility>
+#include <iomanip>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <cassert>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  
+  return 0;
+}
+
+
